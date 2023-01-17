334be8b820d9a28d43c36d153cfe3f1c9f7cbcf3
txt.com <tohir0403>
2023-01-17 12:57:20 +0500

    list_index10

--- bin/list_index10.dart
+++ bin/list_index10.dart
@@ -7,31 +7,14 @@
         list: return answer
 */
 import "dart:math";
-List func(var list_num){
-  var  a = list_num.fold(max);
-  return a;
+List func(List list_num){
+  int a = 0;
+  int s = 0;
+  while(a<list_num.length-1){
+   s = max(list_num[a],list_num[a+1]);
+   a++; 
+  } return [s] ;
 }
 void main() {
-  print (func([2, 6, 9]));
+  print (func([3, 6, 8,12]));
 }
-
-/*
-import "dart:math";
-
-// Main function
-void main() {
-// Creating a geek list
-var geekList = [121, 12, 33, 14, 3];
-	
-// Declaring and assigning the
-// largestGeekValue and smallestGeekValue
-// Finding the smallest and largest value in the list
-var smallestGeekValue = geekList.reduce(min);
-var largestGeekValue = geekList.reduce(max);
-
-// Printing the values
-print("Smallest value in the list : $smallestGeekValue");
-print("Largest value in the list : $largestGeekValue");
-}
-
-*/
